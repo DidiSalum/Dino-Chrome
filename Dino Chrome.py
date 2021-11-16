fe387264b9d4a0bd0161c3775f94bda2c685bcc3
DidiSalum <rodrigo.salum@hotmail.com>
2021-11-16 19:02:03 -0300

    Adicionamos agua
    adicionamos a animação da agua

--- Dino Chrome.py
+++ Dino Chrome.py
@@ -4,51 +4,53 @@ import random
 
 currentimage = 1
 currentfoto = 1
+currentmolhado = 1
 WIDTH = 446
 HEIGHT = 540
 
 garrafa = 250
+aguaX = 100
+aguas = []
 
 
-lista = []
+lista = [Actor("scorpion_1",(223,random.randint(0, 540)))]
 
 
 
 
+def spawn():
+    if len(lista) <= 8:
+        scorpion = Actor("scorpion_1",(223,random.randint(0, 540)))
+        lista.append(scorpion)
 
-
-
-scorpion = Actor("scorpion_1",(223,random.randint(0, 540)))
-lista.append(scorpion)
-scorpion = Actor("scorpion_1",(223,random.randint(0, 540)))
-lista.append(scorpion)
-
-scorpion = Actor("scorpion_1",(223,random.randint(0, 540)))
-lista.append(scorpion)
-
-scorpion = Actor("scorpion_1",(223,random.randint(0, 540)))
-lista.append(scorpion)
-
-scorpion = Actor("scorpion_1",(223,random.randint(0, 540)))
-lista.append(scorpion)
+        
 dino = Actor("dino")
+
 
 def draw ():
     screen.clear()
     screen.fill((0, 187, 255))
     dino.draw()
+
+    for a in aguas:
+        a.draw()
     for s in lista:
         s.draw()
 
 start = True
 def update():
-    global garrafa
+    global garrafa, aguaX
     dino.pos = 100,garrafa
+    aguaX = aguaX + 1
+
     global start
     if start == True:
         clock.schedule_interval(animatedino,0.2)
         clock.schedule_interval(animatescorpion,0.2)
-        start = False
+        clock.schedule_interval(spawn,4.0)
+        clock.schedule_interval(animateagua,1.0)
+    onda()
+    start = False
     if keyboard.w == True:
         garrafa = garrafa - 10
     if keyboard.s == True:
@@ -57,8 +59,19 @@ def update():
         garrafa = 540
     if garrafa == -10:
         garrafa = 0
-    if dino.colliderect(scorpion) == True:
-        dino.image = "dino1"
+    if keyboard.space == True:
+        agua = Actor("agua1")
+        agua.pos = dino.pos
+        aguas.append(agua)
+
+    ggwp = len(lista)
+    for gg in range (ggwp):
+        if dino.colliderect(lista[gg]) == True:
+            dino.image = "dino1"
+        for a in aguas:
+            if a.colliderect(lista[gg]) == True:
+                lista.remove(lista[gg])
+     
 def animatedino():
     global currentimage
     dino.image = "dino" + str(currentimage)
@@ -68,8 +81,15 @@ def animatescorpion():
     for s in lista:
         s.image = "scorpion_" + str(currentfoto)
     currentfoto = currentfoto %7+1
- 
-
+def animateagua():
+    global currentmolhado
+    for a in aguas:
+        a.image = "agua" + str(currentmolhado)
+        currentmolhado = currentmolhado %6+1
+def onda():
+    for a in aguas:
+        a.pos = a.pos[0] + 1, a.pos[1]
+    
 
 
 
